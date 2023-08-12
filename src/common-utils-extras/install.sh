c18f885460acae5b344de8dc627d6ff03d78e448
Audacious Tux <tangimhossain1@gmail.com>
2023-08-12 15:57:04 +0000

    ..

--- src/common-utils-extras/install.sh
+++ src/common-utils-extras/install.sh
@@ -3,12 +3,12 @@
 set -eax
 
 # add repo for gum
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://repo.charm.sh/apt/gpg.key | gpg --dearmor -o /etc/apt/keyrings/charm.gpg
-echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | tee /etc/apt/sources.list.d/charm.list
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
 
-apt-get update -qq
-apt-get install -yqq --no-install-recommends \
+sudo apt-get update -qq
+sudo apt-get install -yqq --no-install-recommends \
     ripgrep \
     bat \
     exa \
