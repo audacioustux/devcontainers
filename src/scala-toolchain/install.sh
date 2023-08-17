b44ddb9073809f4d86b2d1fb0aa5a26426dd8019
Audacious Tux <tangimhossain1@gmail.com>
2023-08-17 11:23:16 +0000

    ..

--- src/scala-toolchain/install.sh
+++ src/scala-toolchain/install.sh
@@ -3,9 +3,9 @@
 set -eax
 
 ARCH=$(uname -m)
-if [[ "$ARCH" == "x86_64" ]]; then
+if [ "$ARCH" == "x86_64" ]; then
     curl -fL "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" | gzip -d > cs
-elif [[ "$ARCH" == "aarch64" ]]; then
+elif [ "$ARCH" == "aarch64" ]; then
     curl -fL "https://github.com/VirtusLab/coursier-m1/releases/latest/download/cs-aarch64-pc-linux.gz" | gzip -d > cs
 else
     echo "Unsupported architecture: $(uname -m)"
