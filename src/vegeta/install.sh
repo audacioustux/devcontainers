9a3b93ea31cf9a26868e3f11fe5e8fa8952ae6d8
Audacious Tux <tangimhossain1@gmail.com>
2023-10-16 17:39:32 +0000

    ..

--- src/vegeta/install.sh
+++ src/vegeta/install.sh
@@ -7,13 +7,15 @@ if [ "$(uname -m)" = "aarch64" ]; then ARCH=arm64; fi
 
 install_vegeta(){
     echo "Installing vegeta..."
+    local repo="tsenart/vegeta"
+    local version=`curl -s https://api.github.com/repos/${repo}/releases/latest | jq -r .tag_name | sed 's/v//'`
     local artifact="vegeta_${version}_linux_${ARCH?}.tar.gz"
-
-    curl -sLO https://github.com/tsenart/vegeta/releases/latest/download/$artifact
-
+    
+    curl -sLO https://github.com/tsenart/vegeta/releases/download/v${version}/${artifact}
+    
     tar xvf $artifact
     rm $artifact
-
+    
     chmod +x vegeta
     mv vegeta /usr/local/bin/vegeta
 }
