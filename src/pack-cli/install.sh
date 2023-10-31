9b32584a7206953478d781b193233a3d8cc49efa
Tanjim Hossain <tangimhossain1@gmail.com>
2023-10-31 04:24:53 +0000

    ..

--- src/pack-cli/install.sh
+++ src/pack-cli/install.sh
@@ -8,10 +8,6 @@ apt-get install -yqq --no-install-recommends software-properties-common
 add-apt-repository ppa:cncf-buildpacks/pack-cli -y
 apt-get install pack-cli
 
-sudo -E -u $_REMOTE_USER bash <<EOF
-    pack config default-builder paketobuildpacks/builder-jammy-base
-EOF
-
 # cleanup
 apt-get clean
 rm -rf /var/lib/apt/lists/*
