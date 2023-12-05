a0d2a1516820cfcce2638e7aa1c766544a079972
Tanjim Hossain <tangimhossain1@gmail.com>
2023-12-05 19:21:38 +0000

    ..

--- src/cargo-bin/install.sh
+++ src/cargo-bin/install.sh
@@ -2,9 +2,11 @@
 
 set -eax
 
-if ${USE_BINSTALL:-false}; then
-    curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-    cargo binstall --no-confirm --no-symlinks ${CRATES:?}
-else
-    cargo install ${CRATES:?}
-fi
+sudo -E -u $_REMOTE_USER zsh <<EOF
+    if ${USE_BINSTALL:-false}; then
+        curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+        cargo binstall --no-confirm --no-symlinks ${CRATES:?}
+    else
+        cargo install ${CRATES:?}
+    fi
+EOF
