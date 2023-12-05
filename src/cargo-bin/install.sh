7bdb691124be0212e392a52a27e95c9eb697d26e
Tanjim Hossain <tangimhossain1@gmail.com>
2023-12-05 19:04:50 +0000

    ..

--- src/cargo-bin/install.sh
+++ src/cargo-bin/install.sh
@@ -2,4 +2,9 @@
 
 set -eax
 
-cargo install ${CRATES:?}
+if ${USE_BINSTALL:-false}; then
+    curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
+    cargo binstall --no-confirm --no-symlinks ${CRATES:?}
+else
+    cargo install ${CRATES:?}
+fi
