32801fd51320668b72fb58a5e26c476a43e8022b
a-0vi <ovidas98@gmail.com>
2024-08-15 09:07:06 +0000

    fix: permission to instlall flyctl

--- src/flyctl/install.sh
+++ src/flyctl/install.sh
@@ -5,8 +5,8 @@ set -eax
 echo "Installing flyctl..."
 sudo -E -u $_REMOTE_USER zsh <<EOF
   if [ "$VERSION" = "latest" ]; then
-    curl -L https://fly.io/install.sh | sh
+    sudo curl -L https://fly.io/install.sh | sh
   else
-    curl -L https://fly.io/install.sh | sh -s "$VERSION"
+    sudo curl -L https://fly.io/install.sh | sh -s "$VERSION"
   fi
 EOF
