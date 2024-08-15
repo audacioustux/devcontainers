28e61a801c4f2d8fdfbf3f4a2befdaeb756ca475
a-0vi <ovidas98@gmail.com>
2024-08-15 15:43:00 +0000

    ugh....

--- src/flyctl/install.sh
+++ src/flyctl/install.sh
@@ -9,9 +9,12 @@ export PATH="$FLYCTL_INSTALL/bin:$PATH"
 
 echo "Installing flyctl..."
 sudo -E -u $_REMOTE_USER zsh <<EOF
-  if [ "$VERSION" = "latest" ]; then
+  export FLYCTL_INSTALL="$USER_HOME/.fly"
+  export PATH="\$FLYCTL_INSTALL/bin:\$PATH"
+  
+  if [ "\$VERSION" = "latest" ]; then
     curl -L https://fly.io/install.sh | sh
   else
-    curl -L https://fly.io/install.sh | sh -s "$VERSION"
+    curl -L https://fly.io/install.sh | sh -s "\$VERSION"
   fi
 EOF
