0cad0b43217d81938bb6e4b921ef322871f880b8
a-0vi <ovidas98@gmail.com>
2024-08-15 18:51:50 +0000

    download the github release of flyctl

--- src/flyctl/install.sh
+++ src/flyctl/install.sh
@@ -2,19 +2,21 @@
 
 set -eax
 
-USER_HOME=$(eval echo ~$_REMOTE_USER)
-
-export FLYCTL_INSTALL="$USER_HOME/.fly"
-export PATH="$FLYCTL_INSTALL/bin:$PATH"
+ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then ARCH=arm64; fi
 
 echo "Installing flyctl..."
-sudo -E -u $_REMOTE_USER zsh <<EOF
-  export FLYCTL_INSTALL="$USER_HOME/.fly"
-  export PATH="\$FLYCTL_INSTALL/bin:\$PATH"
-  
-  if [ "\$VERSION" = "latest" ]; then
-    curl -L https://fly.io/install.sh | sh
-  else
-    curl -L https://fly.io/install.sh | sh -s "\$VERSION"
-  fi
-EOF
+
+# get the download link of github release from the api
+URL=$(curl -s https://api.fly.io/app/flyctl_releases/linux/$ARCH/$VERSION)
+
+# download and extract the binary
+curl -fL $URL | tar -xz
+
+# make the binary executable and create a symlink
+chmod +x flyctl
+ln -s flyctl fly
+
+# move the binary and symlink to /usr/local/bin
+mv fly /usr/local/bin/fly
+mv flyctl /usr/local/bin/flyctl
