c879edab07b6aecdac4254c5bee5aec7b58632fa
a-0vi <ovidas98@gmail.com>
2024-08-15 10:22:31 +0000

    user remoteEnv instead of containerEnv

--- src/flyctl/install.sh
+++ src/flyctl/install.sh
@@ -5,8 +5,8 @@ set -eax
 echo "Installing flyctl..."
 sudo -E -u $_REMOTE_USER zsh <<EOF
   if [ "$VERSION" = "latest" ]; then
-    sudo curl -L https://fly.io/install.sh | sh
+    curl -L https://fly.io/install.sh | sh
   else
-    sudo curl -L https://fly.io/install.sh | sh -s "$VERSION"
+    curl -L https://fly.io/install.sh | sh -s "$VERSION"
   fi
 EOF
